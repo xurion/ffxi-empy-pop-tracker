b2988c3161fd756fa17dd816d50ef8c0c5c678fe
Dean James <xurion@hotmail.com>
2018-12-15 19:20:33 +0000

    Added Glavoid. Tracked NM now saved in settings.xml. Added auto format.

--- nms/briareus.lua
+++ nms/briareus.lua
@@ -5,21 +5,21 @@ return {
       id = 1482, --dented Gigas shield
       from = {
         name = 'Adamastor (North)',
-        pop_item = 2894, --Trophy Shield
+        pop_items = {2894}, --Trophy Shield
       }
     },
     {
       id = 1484, --severed Gigas collar
       from = {
         name = 'Grandgousier (South)',
-        pop_item = 2896, --Massive Armband
+        pop_items = {2896}, --Massive Armband
       }
     },
     {
       id = 1483, --warped Gigas armband
       from = {
         name = 'Pantagruel (East)',
-        pop_item = 2895, --Oversized Sock
+        pop_items = {2895}, --Oversized Sock
       }
     },
   }
---
+++ nms/glavoid.lua
@@ -0,0 +1,49 @@
+return {
+  name = 'Glavoid',
+  data = {
+    {
+      id = 1473, --Sodden sandworm husk
+      from = {
+        name = 'Minhocao (Roams in I-6)',
+      }
+    },
+    {
+      id = 1475, --Sticky gnat wing
+      from = {
+        name = 'Adze (G-5)'
+      }
+    },
+    {
+      id = 1472, --Fat-lined cockatrice skin
+      from = {
+        name = 'Alectryon',
+        pop_items = {2923, 2949}, --Cockatrice Tailmeat, Quivering Eft Egg
+      }
+    },
+    {
+      id = 1474, --Luxuriant manticore mane
+      from = {
+        name = 'Muscaliet',
+        pop_items = {2925, 2950}, --Resilient Mane, Smooth Whisker
+      }
+    },
+  }
+}
+
+--[[
+Minhocao (Roams in I-6)
+  Sodden sandworm husk
+
+Adze (G-5)
+  Sticky gnat wing
+
+Cockatrice Tailmeat
+Quivering Eft Egg
+  Alectryon
+    Fat-lined cockatrice skin
+
+Resilient Mane
+Smooth Whisker
+  Muscaliet
+    Luxuriant manticore mane
+]]
