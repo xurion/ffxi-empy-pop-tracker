68d9ad4ac4d74348987e611c2769a0a029651a6f
Dean James <dean.james6@tesco.com>
2022-08-20 12:47:53 +0100

    Bump version 2.5.0

--- EmpyPopTracker.lua
+++ EmpyPopTracker.lua
@@ -29,7 +29,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 _addon.name = 'Empy Pop Tracker'
 _addon.author = 'Dean James (Xurion of Bismarck)'
 _addon.commands = { 'ept', 'empypoptracker' }
-_addon.version = '2.4.0'
+_addon.version = '2.5.0'
 
 config = require('config')
 res = require('resources')
